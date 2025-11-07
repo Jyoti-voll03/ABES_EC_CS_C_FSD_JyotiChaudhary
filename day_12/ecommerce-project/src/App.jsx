8b4f6d2957f619ef3f63787a28563d2af60349e6
Jyoti-voll03 <chaudharyjyoti2243@gmail.com>
2025-11-07 13:04:07 +0530

    ecommerce-project

---
+++ day_12/ecommerce-project/src/App.jsx
@@ -0,0 +1,73 @@
+import {useEffect} from "react"
+import { useState } from "react"
+import "./app.css"
+const Header = () => {
+  return (
+    <div className="header">Header</div>
+  )
+}
+const Search = ({filterHandler}) => {
+  return (
+    <div className="search">
+      <input type="text" name="search" id="search" onChange={ (e)=>filterHandler(e.target.value)} />
+      <button>search</button>
+    </div>
+  )
+}
+const Product = ({product}) => {
+  return (
+    <div className="product">
+      <img src={product.img}
+        alt="img-here" />
+      <h2>{product.title}</h2>
+      <h3>{product.price}</h3>
+    </div>
+  )
+}
+const Body = () => {
+  const [products, setProducts] = useState([]);
+  const [filteredData,setFilteredData] = useState([]);
+  useEffect(() => {
+    fetch("../products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setFilteredData(data);
+}
+    )
+    .catch((error)=>console.log("unable ot load data",error))
+  }, [])
+  const filterHandler = (query) => {
+    const data = products.filter(
+      (product) => (product.title.toLowerCase().includes(query.toLowerCase()))
+    );
+    setFilteredData(data);
+  }
+      return (
+        <div className="body">
+          <Search filterHandler={filterHandler } />
+          <div className="products">
+            {filteredData.map((product) => <Product product={product } />)}
+          </div>
+        </div>
+        
+      )
+    }
+
+    const Footer = () => {
+      return (
+        <div className="footer">Footer</div>
+      )
+    }
+function App() {
+  
+  return (
+    <div className="app">
+      <Header/>
+        <Body/>
+      <Footer/>
+   </div>
+  )
+}
+
+  export default App
